c7a40cd4fac108010e4f7a7d67b702dbdfacda31
Victor Pinedo <victor.pineda@linea.ai>
2023-11-16 17:18:26 -0500

    Web sever using node

---
+++ web-sever/app.js
@@ -0,0 +1,6 @@
+var http = require('node:http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello world');
+}).listen(1337, '127.0.0.1');
