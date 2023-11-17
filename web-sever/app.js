6f2b2a299c9d6b36a65445c6850b67b4c8fa1b2c
Victor Pinedo <victor.pineda@linea.ai>
2023-11-17 09:31:57 -0500

    Outputting HTML and Templates

--- web-sever/app.js
+++ web-sever/app.js
@@ -1,6 +1,10 @@
 var http = require('node:http');
+var fs = require('node:fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+    var message = 'Hello world...';
+    html = html.replace('{message}', message);
+    res.end(html);
 }).listen(1337, '127.0.0.1');
