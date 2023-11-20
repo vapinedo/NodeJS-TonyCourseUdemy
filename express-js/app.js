1c9650460c2ca79a88efc02623c36d2eed10f244
Victor Pinedo <victor.pineda@linea.ai>
2023-11-20 15:00:15 -0500

    76. Templates and Template Engines

--- express-js/app.js
+++ express-js/app.js
@@ -4,13 +4,15 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 app.use('/', function (req, res, next) {
     console.log('Requested URL: ' + req.url);
     next();
 })
 
 app.get('/', function (req, res) {
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World</h1></body></html>');
+    res.render('index');
 });
 
 app.get('/person/:id', function (req, res) {
